d55140dc64f27dd854c08671eb532b435a117546
Ramhapp Nico <nico.ramhapp@fsw.at>
2020-04-30 13:42:27 +0200

    Projektdateien hinzufügen.

---
+++ FswMobileDeviceMgmt/MitarbeiterIn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FswMobileDeviceMgmt
+{
+    public class MitarbeiterIn
+    {
+        public string Handynummer,Name, Handy, Notebook, Kuerzel;        
+        
+
+
+
+
+        public void WriteAllgemeineInfo()
+        {
+
+            Console.WriteLine("Kürzel: " + Kuerzel + "\nName: " + Name + "\nHandy: " + Handy + "\nHandynummer: " + Handynummer + "\nNB: " + Notebook + "\n");
+
+        }
+    }
+}
---
+++ FswMobileDeviceMgmt/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace FswMobileDeviceMgmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MitarbeiterIn wolfgang = new MitarbeiterIn();
+            
+            wolfgang.Kuerzel = "ifswwha";
+            wolfgang.Name = "Wolfgang Hans";
+            wolfgang.Handy = "Iphone8";
+            wolfgang.Handynummer = "0660 1234 556";
+            wolfgang.Notebook = "203456";
+
+            MitarbeiterIn peter = new MitarbeiterIn();
+
+            peter.Kuerzel = "ifswplu";
+            peter.Name = "Peter Ludwig";
+            peter.Handy = "Iphone8";
+            peter.Handynummer = "0650 9898 334";
+            peter.Notebook = "203457";
+
+            MitarbeiterIn alexandra = new MitarbeiterIn();
+
+            alexandra.Kuerzel = "ifswasa";
+            alexandra.Name = "Alexandra Sabine";
+            alexandra.Handy = "IphoneSE";
+            alexandra.Handynummer = "0650 5656 900";
+            alexandra.Notebook = "203458";
+
+
+            //eingabe
+            string eingabe;
+            
+            
+            do
+            {
+
+                Console.WriteLine("Please enter Name or \"exit\" \n");
+                
+                eingabe = Console.ReadLine();
+
+
+                if (eingabe == "Wolfgang")
+                {
+                    wolfgang.WriteAllgemeineInfo();
+                }
+
+
+                else if (eingabe == "Peter")
+                {
+                    peter.WriteAllgemeineInfo();
+                }
+
+
+                else if (eingabe == "Alexandra")
+                {
+                    alexandra.WriteAllgemeineInfo();
+                }
+
+                else if (eingabe.Equals("Exit", StringComparison.InvariantCultureIgnoreCase))
+                {
+                   break;
+                }                
+
+                else
+                {
+                    Console.WriteLine("\nFalse Name\n");
+                                       
+                }
+
+            } while (true);
+        }
+    }
+}
